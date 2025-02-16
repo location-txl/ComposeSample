5b93e0f754a142be460c84dd6b4394d75decc329
tianxiaolong <tttx0307@163.com>
2025-02-16 23:37:30 +0800

    feat(ScreenRecompositionSample):增强屏幕重组示例功能- 添加了对不稳定数据导致重组的详细解释和示例 - 新增 Strong Skipping 模式相关说明和演示 - 增加了跳过重组和不跳过重组的对比示例 - 优化了界面布局和交互，增加了预览功能

--- app/src/main/java/com/location/compose/sample/other/recomposition/ScreenRecompositionSample.kt
+++ app/src/main/java/com/location/compose/sample/other/recomposition/ScreenRecompositionSample.kt
@@ -1,5 +1,6 @@
 package com.location.compose.sample.other.recomposition
 
+import android.R.attr.onClick
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,10 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.location.compose.sample.common.HorizontalDivide
 import com.location.compose.sample.common.TitleBar
 import com.location.compose.sample.common.randomColor
+import java.util.UUID
 
 
 @Composable
@@ -128,26 +131,76 @@ private fun NoComposable() {
 private fun ComposableParam(value: String){
     Text(text = "value:$value", color = randomColor)
 }
+private var userData =   UserData("hello")
 
 @Composable
 private fun NoStableDataComposable(){
-    var userData = remember { UserData("hello") }
     HorizontalDivide()
-    Text("不稳定的数据会导致一直重组 无法跳过重组", style = MaterialTheme.typography.titleMedium)
+    Text("不稳定的数据会使用===比较 而不是比较equal", style = MaterialTheme.typography.titleMedium)
+    Text("""
+        不稳定的数据会使用===比较 而不是比较equal
+        Strong Skipping 是compose 新增的一种模式 启用时不稳定的数据可以跳过 不稳定的lambda 会被记住
+        在 Kotlin 2.0.20 中默认启用强跳过。https://developer.android.com/develop/ui/compose/performance/stability/strongskipping
+        开启后 如果一个数据的引用和值没变 则不会触发重组
+    """.trimIndent(), style = MaterialTheme.typography.bodyLarge)
     var count by remember { mutableIntStateOf(0) }
+    Text("可以跳过")
+    NoStableText(userData.name)
+    Text("不可以跳过 加上 @NonSkippableComposable 注解")
+    NoStableRestartText(userData.name)
+    Text("count:$count")
+
     Button(
         onClick = {
             count++
-            userData.name = "value-$count"
+            userData.name = "hello-$count"
         }
     ) {
-        Text(text = "修改 count$count")
+        Text(text = "修改不稳定的对象", modifier = Modifier.randomColor())
     }
-
-
+    Button(onClick = {
+        count++
+    }) {
+        Text(text = "只修改Count", modifier = Modifier.randomColor())
+    }
 }
 
 
-private data class UserData(var name: String)
+
+
+@Composable
+private fun NoStableText(name: String){
+    Text("name:${name}", color = randomColor)
+}
+@NonSkippableComposable
+@Composable
+private fun NoStableRestartText(name: String){
+    Text("name:${name}", color = randomColor)
+}
+
+private  class UserData(var name: String)
+
+
+
+
 
 private fun Modifier.randomColor() = this.then(Modifier.background(randomColor))
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ScreenRecompositionSamplePreview() {
+    ScreenRecompositionSample {
+
+    }
+
+//    Sample()
+}
+
+
+
+
+
+//现在相等就永远相等
+//当公开属性改变的时候，通知到用到这个属性的Composition
+//公开属性需要全部是稳定/可靠属性
